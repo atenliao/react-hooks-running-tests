a21ca566ad4985eef3ff5ad2056fca8eb516afee
Zhongming <atenliao@gmail.com>
2023-05-16 21:43:23 -0700

    update test.js file and component files

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,6 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  const element = screen.queryByText("please pass this test")
+  expect(element).toBeInTheDocument();
 });
